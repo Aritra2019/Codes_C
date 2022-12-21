8116d8ec9d8a7311f43b33e88f6a1494cd70a26a
Aritra <aritra.b.2002@gmail.com>
2022-12-22 01:02:45 +0530

    Hello

---
+++ Pattern.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    for (int i = 0; i < 11; i++)
+    {
+        if(i>0 && i<10)
+            {
+              if(j)   
+            }
+        for(int j=0;j<11;j++)
+        {
+            if(j>0 && j<10)
+            {
+              if(j)   
+            }
+        }
+    }
+    
+}
